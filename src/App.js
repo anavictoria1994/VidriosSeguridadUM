edf1b7c3fb9d49a154e91aad58b09c3c5a3b028c
anavictoria1994 <anavictoriaacu@gmail.com>
2021-08-18 19:33:11 -0500

    Reparacion interfaz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
 import * as ReactBootStrap from "react-bootstrap";
-import vidrio from './assets/img/vidrio.jpg';
+import division1 from './assets/img/divin1.png';
+import pasamanos1 from './assets/img/pasamanos.jpg';
+import espejos from './assets/img/espejos.jpg';
 import mision from './assets/img/mision.jpg';
 import vision from './assets/img/vision.png';
+import logo from './assets/img/UMLOGO.png';
 import objetivo from './assets/img/objetivo.jpg';
 import './components/styles/carousel.css';
 
@@ -11,19 +14,17 @@ function App() {
   return (
     <div className="App">
       <div>
-      <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
-      <ReactBootStrap.Navbar.Brand href="#inicio">Vidrios Seguridad UM</ReactBootStrap.Navbar.Brand>
+      <ReactBootStrap.Navbar collapseOnSelect expand="lg" variant="dark" className="colornavbar">
+      <ReactBootStrap.Navbar.Brand  href="#inicio">Vidrios Seguridad UM</ReactBootStrap.Navbar.Brand>
       <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
         <ReactBootStrap.Nav className="mr-auto">
         <ReactBootStrap.Nav.Link href="#inicio">Inicio</ReactBootStrap.Nav.Link>
         <ReactBootStrap.Nav.Link href="#contacto">Contacto</ReactBootStrap.Nav.Link>
         <ReactBootStrap.NavDropdown title="Productos" id="collasible-nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Costos</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Seguridad</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Vidrios</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Divider />
-        <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Divisiones de Baños</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Pasamanos</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Ventanas</ReactBootStrap.NavDropdown.Item>
         </ReactBootStrap.NavDropdown>
         </ReactBootStrap.Nav>
         <ReactBootStrap.Nav>
@@ -36,40 +37,37 @@ function App() {
       </ReactBootStrap.Navbar>
       </div>
       <div className="margi">
-      <ReactBootStrap.Carousel >
+      <ReactBootStrap.Carousel>
         <ReactBootStrap.Carousel.Item>
           <img
             className="carousel"
-            src ={vidrio}
+            src ={division1}
             alt="Second slide"
           />
           <ReactBootStrap.Carousel.Caption>
-            <h3>VIDRIOS</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>DIVISIONES DE BAÑOS</h3>
           </ReactBootStrap.Carousel.Caption>
         </ReactBootStrap.Carousel.Item>
         <ReactBootStrap.Carousel.Item>
           <img 
             className="carousel"
-            src ={vidrio}
+            src ={espejos}
             alt="Second slide"
             
           />
           <ReactBootStrap.Carousel.Caption>
-            <h3>BAÑOS</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3>ESPEJOS</h3>
           </ReactBootStrap.Carousel.Caption>
         </ReactBootStrap.Carousel.Item>
         <ReactBootStrap.Carousel.Item>
           <img
             className="carousel"
-            src ={vidrio}
+            src ={pasamanos1}
             alt="Second slide"
           />
 
           <ReactBootStrap.Carousel.Caption>
-            <h3>VENTANAS</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+            <h3>PASAMANOS</h3>
           </ReactBootStrap.Carousel.Caption>
         </ReactBootStrap.Carousel.Item>
       </ReactBootStrap.Carousel>
@@ -92,7 +90,7 @@ function App() {
             </div>
             <div class="col-12 col-sm-4 text-centar">
               
-              <h5 class="altura-texto">VISION </h5>
+              <h5 class="altura-texto">VISION</h5>
               <img
                 className="carousel"
                 src ={vision}
@@ -114,32 +112,30 @@ function App() {
             </div>
           </div>
       </section>
-      <section class="altura-a-b">
-        <div class="container">
-          <div class="embed-responsive embed-responsive-21by9">
-          </div>
-        </div>
-      </section>
      </main>
     </div>
 
       <div>
-        <footer>
-      <   div class="row m-0 col-md-12" >
+        <footer className="footer">
+      <   div class="row m-0 col-md-12 align-items-center">
             <div class="d-inline-block col-md-4" >
+            <img
+            className="logo"
+            src ={logo}
+          />
             </div>
-            <div class="d-inline-block col-md-4">
-              <h5 >QUÍENES SOMOS</h5>
-              <p>Somos un equipo de profesionales apasionados cuya meta
+            <div class="d-inline-block col-md-4" >
+              <h5 className="letras">QUÍENES SOMOS</h5>
+              <p className="letras">Somos un equipo de profesionales apasionados cuya meta
                 es brindar la mejor atención a todas aquellas personas 
                 que lo necesitan y estamos dispuestos a dar lo mejor 
                 para lograrlo.</p>
             </div>
             <div class="d-inline-block col-md-4">
-              <h5 > CONTÁCTESE CON NOSOTROS </h5>
-              <p>+57 319 569 896 /
-                425 36 96</p>
-              <p>stayathome2020@gmail.com</p>
+              <h5 className="letras"> CONTÁCTESE CON NOSOTROS </h5>
+              <p className="letras">+57 316 712 0802 /
+                318 401 2623</p>
+              <p className="letras">vidrioseguridadum@gmail.com</p>
               <p></p>
             </div>
           </div>
